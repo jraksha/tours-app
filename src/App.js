716ff93475aedc024597835019f0b2f712addde0
rjain@deqode <rjain@deqode.com>
2023-03-13 18:39:54 +0530

    tour-app

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+import data from "./data";
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+// const url = 'https://course-api.com/react-tours-project'
+
+function App() {
+  const [loading, setLoading] = useState(false);
+  const [tours, setTours] = useState(data);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
+  const fetchTours = () => {
+    setLoading(true);
+  };
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>No tours left</h2>
+          <button className="btn" onClick={fetchTours}>
+            Refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  );
+}
+
+export default App;
